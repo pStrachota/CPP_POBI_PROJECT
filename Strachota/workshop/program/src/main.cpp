b5943e526eac86fef80da03c79cd95b7c381f939
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2021-03-15 22:42:49 +0100

    Initial Commit

---
+++ Strachota/workshop/program/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "math_helpers.h"
+
+using namespace std;
+
+int main() {
+
+    cout << factorial(5) << endl;
+
+    return 0;
+}
---
+++ Wieczorek/workshop/build/program/CMakeFiles/Program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program.dir/src/main.cpp.o"
+  "CMakeFiles/Program.dir/__/library/src/math_helpers.cpp.o"
+  "CMakeFiles/Program.dir/src/math_helpers.cpp.o"
+  "Program.pdb"
+  "Program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
