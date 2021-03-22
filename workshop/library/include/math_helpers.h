8a67f323d277b81cd84f3be7acbc515b083cf218
236660 Strachota Piotr <236660@edu.p.lodz.pl>
2021-03-22 19:24:05 +0100

    usuwanie zbednego workshopa

--- workshop/library/include/math_helpers.h
+++
@@ -1,6 +0,0 @@
-/**
- * @brief Oblicza silnię dla danego parametru n.
- * @param n - argument do obliczenia wartości silni.
- * @return Wartość silni dla danego parametru n.
- */
-unsigned long long factorial(unsigned int n);
