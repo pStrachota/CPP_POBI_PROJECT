84e352ca967cf8dc3b67c001d0b02033dcee1e2e
236691 Wieczorek Paweł <236691@edu.p.lodz.pl>
2021-04-18 18:47:07 +0200

    poczatek

---
+++ pwieczorek/workshop/library/include/model/Client.h
@@ -0,0 +1,31 @@
+//
+// Created by student on 18.04.2021.
+//
+
+#ifndef CARRENTAL_CLIENT_H
+#define CARRENTAL_CLIENT_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+class Client {
+
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string personalID;
+public:
+    Client();
+    Client(const std::string &firstName, const std::string &lastName, const std::string &personalID);
+    ~Client();
+    std::string getClientInfo();
+    void setFirstName(const std::string & ) ;
+    void setLastName(const std::string & ) ;
+    const std::string & getFirstName() const;
+    const std::string & geLastName() const;
+    const std::string & getPersonalID() const;
+
+};
+
+#endif //CARRENTAL_CLIENT_H
--- pwieczorek/workshop/program/src/main.cpp
+++ pwieczorek/workshop/program/src/main.cpp
@@ -1,15 +1,26 @@
 //
 // Created by student on 13.03.2021.
 //
+
 #include <iostream>
-#include "math_helpers.h"
+#include "model/Client.h"
+
 using namespace std;
 
 int main(int argc,char **argv)
 {
-    int n = 5;
-    cout << factorial(n) << endl;
-    cout<<"Hello World"<<endl;
+    cout << "test obiektów:" << endl;
+    Client c1("Pawel","Kowalski","7770777");
+    Client c2("Gawel","Kowalski","7770777");
 
+    Client *wskC = new Client;
+    wskC = &c2;
+
+    cout << wskC->getClientInfo() << endl;
+    cout << c1.getClientInfo() << endl;
+
+    c1.setFirstName("");
+    c1.setFirstName("NoweImiePawla");
+    cout << c1.getClientInfo();
     return 0;
 }
