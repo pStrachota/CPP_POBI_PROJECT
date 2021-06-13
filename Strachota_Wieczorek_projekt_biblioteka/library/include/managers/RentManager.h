c728dc37744bc4974a9a5955455661ba8ab7fc3f
Student IT <236691@edu.p.lodz.pl>
2021-06-13 20:55:10 +0200

    nowe komentarze

--- Strachota_Wieczorek_projekt_biblioteka/library/include/managers/RentManager.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/managers/RentManager.h
@@ -10,6 +10,10 @@
 #include <boost/date_time.hpp>
 #include "model/Rent.h"
 
+/**
+ * @brief RentManager zarzadza repozytorium wypozyczen
+ */
+
 namespace pt = boost::posix_time;
 namespace gr = boost::gregorian;
 
@@ -19,10 +23,32 @@ class RentManager {
 public:
     RentPtr rentRentableItem(const ClientPtr &client, const RentableItemPtr &rentableItem,pt::ptime beginTime);
     void removeRentableItem(RentableItemPtr& rentableItem);
+    /**
+     * @brief getRentableItemRent to getter wypozyczenia
+     * @param entableItem - fizyczny obiekt przypisany do wypozyczenia
+     * @return wskaznik na wypozyczenie
+     */
     RentPtr getRentableItemRent(const RentableItemPtr &rentableItem);
+    /**
+     * @brief findRents znajduje wypozyczenia wedlug predykatu
+     * @param predicate - predykat
+     * @return wektor wkaznikow na wypozyczenia
+     */
     std::vector<RentPtr> findRents(const RentPredicate& predicate);
+    /**
+     * @brief getAllClientRents znajduje wypozyczenia danego klienta
+     * @param client - wskaznik na klienta
+     * @return wektor wkaznikow na wypozyczenia
+     */
     std::vector<RentPtr> getAllClientRents(const ClientPtr &client);
+    /**
+     * @brief countRents zlicza wszystkie wypozyczenia biblioteki
+     * @return int
+     */
     int countRents() const;
+    /**
+     * @brief saveRentsToFileByPredicate zapisuje wypozyczenia spelniajace dany predykat do pliku
+     */
     void saveRentsToFileByPredicate(const RentPredicate &);
 
 };
