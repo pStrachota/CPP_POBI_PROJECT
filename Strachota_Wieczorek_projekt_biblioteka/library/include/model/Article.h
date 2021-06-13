65e7078187e16bc2785f11684ded359fb5fd1248
Student IT <236691@edu.p.lodz.pl>
2021-06-13 20:20:27 +0200

    komentarze

--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/Article.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/Article.h
@@ -5,12 +5,25 @@
 
 #include "RentableItem.h"
 
+/**
+ * @brief klasa Article reprezentuje artykuł naukowy
+ * dziedziczy z RentableItem
+ */
+
 class Article : public RentableItem {
 private:
     std::string parentOrganisation;
 public:
     Article(const std::string &serialNumber, const std::string &author, const std::string &title, const std::string &parentOrganisation);
+    /**
+   * @brief getInfo konkatenuje informacje artykule
+   * @return string zawierający informacje o artykule
+   */
     std::string getInfo() const override;
+    /**
+   * @brief getter dla organizacji macierzystej artkykułu
+   * @return string zwraza informacje o organizacji
+   */
     const std::string &getParentOrganisation() const;
 };
 
--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/Book.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/Book.h
@@ -4,12 +4,25 @@
 
 #include "RentableItem.h"
 
+/**
+ * @brief klasa Book reprezentuje ksiazke
+ * dziedziczy z RentableItem
+ */
+
 class Book : public RentableItem{
 private:
     std::string publishingHouse;
 public:
     Book(const std::string &serialNumber, const std::string &author, const std::string &title, const std::string &publishingHouse);
+    /**
+   * @brief getInfo konkatenuje informacje ksiazce
+   * @return string zawierający informacje o ksiazce
+   */
     std::string getInfo() const override;
+    /**
+   * @brief getter dla wydawnictwa
+   * @return string zwraza informacje o wydawnictwie
+   */
     const std::string &getPublishingHouse() const;
 };
 
