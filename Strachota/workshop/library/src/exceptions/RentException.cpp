d6900fd21fd2227797daeaeb820d6c9d799561a4
pStrachota <81098347+pStrachota@users.noreply.github.com>
2022-02-28 18:51:51 +0100

    Delete Strachota directory

--- Strachota/workshop/library/src/exceptions/RentException.cpp
+++
@@ -1,9 +0,0 @@
-#include "exceptions/RentException.h"
-
-RentException::RentException(const std::string& arg) : logic_error(arg)
-{}
-
-const std::string RentException::exceptionClientNullPtr = "INVALID CLIENT (NULL)";
-const std::string RentException::exceptionVehicleNullPtr = "INVALID VEHICLE (NULL)";
-const std::string RentException::exceptionDays = "INVALID NUMBER OF DAYS TO SUBSTRACT (NEGATIVE VALUE)";
-
--- Strachota/workshop/library/src/exceptions/VehicleException.cpp
+++
@@ -1,9 +0,0 @@
-#include "exceptions/VehicleException.h"
-
-VehicleException::VehicleException(const std::string& arg) : logic_error(arg)
-{
-}
-
-const std::string VehicleException::exceptionPrice = "INVALID PRICE";
-const std::string VehicleException::exceptionRegistration = "INVALID REGISTRATION NUMBER";
-
