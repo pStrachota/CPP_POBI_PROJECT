69225a95c587853d9c1f5acc72be4b4de86e2bfc
pStrachota <81098347+pStrachota@users.noreply.github.com>
2022-02-28 18:51:39 +0100

    Delete pwieczorek directory

--- pwieczorek/introduction/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/CLionProjects/introduction
-# Build directory: /home/student/CLionProjects/introduction/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("program")
-subdirs("library")
--- pwieczorek/workshop/library/include/math_helpers.h
+++
@@ -1,6 +0,0 @@
-#ifndef CARRENTAL_MATH_HELPERS_H
-#define CARRENTAL_MATH_HELPERS_H
-
-unsigned long long factorial(unsigned int n);
-
-#endif //CARRENTAL_MATH_HELPERS_H
