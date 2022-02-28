a23891549c60c516d78964cd6f85328de26b47d8
pStrachota <81098347+pStrachota@users.noreply.github.com>
2022-02-28 18:52:04 +0100

    Delete Strachota_Wieczorek_projekt_biblioteka/build directory

--- Strachota_Wieczorek_projekt_biblioteka/build/library/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/projekt/Strachota_Wieczorek_projekt_biblioteka/library
-# Build directory: /home/student/projekt/Strachota_Wieczorek_projekt_biblioteka/build/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LibraryTest1 "LibraryTest" "--report_level=detailed" "--log_level=all" "--color_output=yes")
