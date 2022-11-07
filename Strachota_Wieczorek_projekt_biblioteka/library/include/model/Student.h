0613790ed311a4bfe4346ca52e8588e94124d149
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 22:01:43 +0100

    Update Student.h

--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/Student.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/Student.h
@@ -15,28 +15,13 @@
 class Student : public ClientType {
 
 public:
-    /**
-* @brief getMaxDays zwaca maksymalna liczbe dni wypozyczen ksiazki lub artyukulu
-    * dla studenta wynosi: 20 dni
-* @return int
-*/
+ 
     int getMaxDays() const override;
-    /**
-    * @brief getMaxitems zwaca maksymalna liczbe wypozyczen danego typu klienta
-    * dla studenta wynosi: 2 przedmioty
-    * @return int
-    */
+   
     int getMaxItems() const override;
-    /**
-       * @brief getter dla kary za przekroczenie wypozyczenia
-       * dla studenta wynosi: 50gr za każdy przekroczony dzień
-       * @return double
-       */
+   
     double getPenalty() const override;
-    /**
-      * @brief getTypeInfo zwraca informacje o typie klienta
-      * @return wartosc typu string
-      */
+   
     std::string getTypeInfo() const override;
 
     virtual ~Student();
