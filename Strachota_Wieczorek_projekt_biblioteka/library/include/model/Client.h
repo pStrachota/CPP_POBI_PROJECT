b42b0c6fa82bd213f0db19a43b2bf4b54f16ea9a
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 21:57:47 +0100

    Update Client.h

--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/Client.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/Client.h
@@ -48,71 +48,31 @@ public:
            const AddressPtr &address, const ClientTypePtr &clientType);
     const std::string &getName() const;
     void setClientType(const ClientTypePtr &clientType);
-    /**
-    * @brief getInfo konkatenuje i zwraca informacje o kliencie
-    * @return wartosc typu string
-    */
+    
     std::string getInfo() const;
-    /**
-   * @brief setter dla "bycia w archiwum"
-   * @param archive
-   */
+   
     void setArchive(bool archive);
-    /**
-   * @brief setter dla imienia
-   * @param name
-   */
+    
     void setName(const std::string &name);
-    /**
-   * @brief setter dla nazwiska
-   * @param surname
-   */
+    
     void setSurname(const std::string &surname);
-    /**
-   * @brief setter dla peselu
-   * @param personalId
-   */
+   
     void setPersonalId(const std::string &personalId);
-    /**
-   * @brief setter dla wskaznika na adres
-   * @param address
-   */
+    
     void setAddress(const AddressPtr &address);
-    /**
-  * @brief getter dla nazwiska
-  * @param surname
-  */
+    
     const std::string &getSurname() const;
-    /**
-  * @brief setter dla umieszczenia w archiwum
-  * @param archive
-  */
+    
     bool isArchive() const;
-    /**
-  * @brief getter dla personalId (peselu)
-  * @param personalId
-  */
+    
     const std::string &getId() const;
-    /**
-  * @brief getter dla wskaznika na adres
-  * @param address
-  */
+   
     const AddressPtr &getAddress() const;
-    /**
-   * @brief getMaxDays zwaca maksymalna liczbe dni wypozyczen ksiazki lub artyukulu
-   * @return int
-   */
+    
     int getMaxDays() const;
-    /**
-   * @brief getMaxitems zwaca maksymalna liczbe wypozyczen danego typu klienta
-   * @return int
-   */
+    
     int getMaxItems() const;
-    /**
-     * @brief getter dla kary za przekroczenie wypozyczenia
-     * rozny dla roznych typow klienta
-     * @return double
-     */
+   
     double getPenalty() const;
 
 };
