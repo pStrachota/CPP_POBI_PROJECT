48b82a69fb8e71d0bcaa08d3621056be4b2f3661
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 21:51:46 +0100

    Update typedefs.h

--- Strachota_Wieczorek_projekt_biblioteka/library/include/typedefs.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/typedefs.h
@@ -27,29 +27,20 @@ class ObserverC;
 class ObserverR;
 
 typedef std::shared_ptr<Client> ClientPtr;
+typedef std::shared_ptr<ClientType> ClientTypePtr;
+
+typedef std::shared_ptr<Rent> RentPtr;
+typedef std::shared_ptr<Address> AddressPtr;
+typedef std::shared_ptr<StorageContainer> ContainerPtr;
+typedef std::shared_ptr<RentableItem> RentableItemPtr;
+
 typedef std::shared_ptr<Observer> ObserverPtr;
 typedef std::shared_ptr<ObserverC> Observer2Ptr;
 typedef std::shared_ptr<ObserverR> Observer3Ptr;
-typedef std::shared_ptr<ClientType> ClientTypePtr;
-typedef std::shared_ptr<Rent> RentPtr;
-//typedef std::shared_ptr<Vehicle> VehiclePtr;
-typedef std::shared_ptr<Address> AddressPtr;
-//typedef std::shared_ptr<Book> BookPtr;
-//typedef std::shared_ptr<Article> ArticlePtr;
-typedef std::shared_ptr<StorageContainer> ContainerPtr;
-
-typedef std::shared_ptr<RentableItem> RentableItemPtr;
-
-//typedef std::shared_ptr<VehicleRepository> VehicleRepoPtr;
-//typedef std::shared_ptr<RentRepository> RentRepoPtr;
-//typedef std::function<bool(VehiclePtr)> VehiclePredicate;
 
 typedef std::function<bool(ClientPtr)> ClientPredicate;
 typedef std::function<bool(AddressPtr)> AddressPredicate;
-
 typedef std::function<bool(RentPtr)> RentPredicate;
 typedef std::function<bool(RentableItemPtr)> RentableItemPredicate;
-
-
 
 #endif //STRACHOTA_WIECZOREK_PROJEKT_BIBLIOTEKA_TYPEDEFS_H
