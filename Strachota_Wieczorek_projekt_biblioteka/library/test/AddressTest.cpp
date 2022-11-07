d101453dee22546a47ccc26d345c643fbf3f9f51
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 22:07:14 +0100

    Update AddressTest.cpp

--- Strachota_Wieczorek_projekt_biblioteka/library/test/AddressTest.cpp
+++ Strachota_Wieczorek_projekt_biblioteka/library/test/AddressTest.cpp
@@ -5,10 +5,10 @@
 BOOST_AUTO_TEST_SUITE(TestSuiteAddress)
 
     BOOST_AUTO_TEST_CASE(ParameterConstructorTests) {
-        AddressPtr address = std::make_shared<Address>("Wicie", "Fajna", "420");
-        BOOST_TEST(address->getCity() == "Wicie");
-        BOOST_TEST(address->getStreet() == "Fajna");
-        BOOST_TEST(address->getNumber() == "420");
+        AddressPtr address = std::make_shared<Address>("Warszawa", "Kwiatowa", "200");
+        BOOST_TEST(address->getCity() == "Warszawa");
+        BOOST_TEST(address->getStreet() == "Kwiatowa");
+        BOOST_TEST(address->getNumber() == "200");
     }
 
 BOOST_AUTO_TEST_SUITE_END()
