f354217014083bc6da73396c20bda902b98cee99
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 21:56:24 +0100

    Update Article.h

--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/Article.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/Article.h
@@ -15,15 +15,9 @@ private:
     std::string parentOrganisation;
 public:
     Article(const std::string &serialNumber, const std::string &author, const std::string &title, const std::string &parentOrganisation);
-    /**
-   * @brief getInfo konkatenuje informacje artykule
-   * @return string zawierający informacje o artykule
-   */
+  
     std::string getInfo() const override;
-    /**
-   * @brief getter dla organizacji macierzystej artkykułu
-   * @return string zwraza informacje o organizacji
-   */
+    
     const std::string &getParentOrganisation() const;
 
     virtual ~Article();
