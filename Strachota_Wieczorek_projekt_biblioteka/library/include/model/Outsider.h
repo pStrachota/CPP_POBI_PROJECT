ad55ce8072f5c6f7eb0cc4673c6233db01ea6a64
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 22:00:14 +0100

    Update Outsider.h

--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/Outsider.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/Outsider.h
@@ -13,28 +13,12 @@
 
 class Outsider : public ClientType{
 public:
-    /**
- * @brief getMaxDays zwaca maksymalna liczbe dni wypozyczen ksiazki lub artyukulu
-     * dla outsidera wynosi: 10 dni
- * @return int
- */
     int getMaxDays() const override;
-    /**
-     * @brief getMaxitems zwaca maksymalna liczbe wypozyczen danego typu klienta
-     * dla outsidera wynosi: 1 przedmiot
-     * @return int
-     */
+    
     int getMaxItems() const override;
-    /**
-       * @brief getter dla kary za przekroczenie wypozyczenia
-       * dla outsidera wynosi: 1zł za każdy przekroczony dzień
-       * @return double
-       */
+   
     double getPenalty() const override;
-    /**
-      * @brief getTypeInfo zwraca informacje o typie klienta
-      * @return wartosc typu string
-      */
+  
     std::string getTypeInfo() const override;
 
     virtual ~Outsider();
