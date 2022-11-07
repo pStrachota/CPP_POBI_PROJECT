7c354f9de5af6a60b79ec57eaae78dfe1587c2fe
Piotr Strachota <81098347+pStrachota@users.noreply.github.com>
2022-11-07 22:01:18 +0100

    Update RentableItem.h

--- Strachota_Wieczorek_projekt_biblioteka/library/include/model/RentableItem.h
+++ Strachota_Wieczorek_projekt_biblioteka/library/include/model/RentableItem.h
@@ -21,35 +21,17 @@ public:
 
 public:
     RentableItem(const std::string &serialNumber, const std::string &author, const std::string &title);
-    /**
-    * @brief getter informacji o wypozyczeniu
-    * @return string
-    */
+   
     virtual std::string getInfo() const = 0;
-    /**
-    * @brief setter dla "bycia w archiwum"
-    * @param archive
-    */
+   
     void setArchive(bool archive);
-    /**
-    * @brief getter ID przedmiotu
-    * @return string
-    */
+   
     const std::string &getId() const;
-    /**
-    * @brief getter autora przedmiotu
-    * @return string
-    */
+   
     const std::string &getAuthor() const;
-    /**
-    * @brief getter tytułu przedmiotu
-    * @return string
-    */
+    
     const std::string &getTitle() const;
-    /**
-    * @brief flaga czy zarchiwizowane
-    * @return bool
-    */
+    
     bool isArchive() const;
 };
 
